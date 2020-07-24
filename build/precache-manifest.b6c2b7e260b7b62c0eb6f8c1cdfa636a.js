7a27b807413cde3ce4d9ca8ec6d4e74a016f02ec
Reid Plumlee <reidjplumlee@gmail.com>
2020-07-24 16:10:08 -0700

    deploy

---
+++ build/precache-manifest.b6c2b7e260b7b62c0eb6f8c1cdfa636a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3dbb5eb9bbf6185094d0ba9ed5703903",
+    "url": "/ppdemo/index.html"
+  },
+  {
+    "revision": "3ab47492763ba1e4136b",
+    "url": "/ppdemo/static/css/main.2f08fb37.chunk.css"
+  },
+  {
+    "revision": "49fe3e8bc9e8ed69a768",
+    "url": "/ppdemo/static/js/2.8aa65b28.chunk.js"
+  },
+  {
+    "revision": "882fee5ad56cfd78b1cc5d94b617aa42",
+    "url": "/ppdemo/static/js/2.8aa65b28.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ab47492763ba1e4136b",
+    "url": "/ppdemo/static/js/main.8959d8d1.chunk.js"
+  },
+  {
+    "revision": "fd1b2cd8d51e00e01d29",
+    "url": "/ppdemo/static/js/runtime-main.f059ed62.js"
+  },
+  {
+    "revision": "3e6de7d1ee7f9345b104cb2d3d37453b",
+    "url": "/ppdemo/static/media/parallax.3e6de7d1.jpg"
+  }
+]);
