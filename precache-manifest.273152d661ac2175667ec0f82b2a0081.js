4248e0eb16f2fef0eadd29bb2144eb0f8e18c152
Reid Plumlee <reidjplumlee@gmail.com>
2020-09-18 15:59:23 -0700

    Updates

--- precache-manifest.273152d661ac2175667ec0f82b2a0081.js
+++ precache-manifest.273152d661ac2175667ec0f82b2a0081.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "d453fd65ab4d38cff903066fbefe0291",
+    "revision": "42f7ebf80f802ef70b86c9ce7bd35793",
     "url": "/ppdemo1/index.html"
   },
   {
-    "revision": "3f043be9b2f6c9ea3150",
-    "url": "/ppdemo1/static/css/main.e65f2dc3.chunk.css"
+    "revision": "5f9701a55a20ba4ab9c5",
+    "url": "/ppdemo1/static/css/main.3eacbb4b.chunk.css"
   },
   {
-    "revision": "ee74721d5f84a5eeb1cc",
-    "url": "/ppdemo1/static/js/2.4f268e51.chunk.js"
+    "revision": "82307a369535a7379950",
+    "url": "/ppdemo1/static/js/2.15ec7658.chunk.js"
   },
   {
     "revision": "882fee5ad56cfd78b1cc5d94b617aa42",
-    "url": "/ppdemo1/static/js/2.4f268e51.chunk.js.LICENSE.txt"
+    "url": "/ppdemo1/static/js/2.15ec7658.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "3f043be9b2f6c9ea3150",
-    "url": "/ppdemo1/static/js/main.8e9b45eb.chunk.js"
+    "revision": "5f9701a55a20ba4ab9c5",
+    "url": "/ppdemo1/static/js/main.38447c63.chunk.js"
   },
   {
     "revision": "6e9e0c76d78997824aa6",
